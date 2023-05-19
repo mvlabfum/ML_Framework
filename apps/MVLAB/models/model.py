9d2595bc300be1bfdee8a7bf9e2b8c5770dd6313
Ali Hejrati <101740277+alihejrati@users.noreply.github.com>
2023-05-19 20:29:00 +0330

    Delete apps/MVLAB directory

--- apps/MVLAB/models/model.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
-from utils.pt.nnModuleBase import nnModuleBase
-from utils.pl.plModuleBase import plModuleBase
-from libs.basicIO import signal_save, compressor
-
-class MVLAB(plModuleBase):
-    pass
