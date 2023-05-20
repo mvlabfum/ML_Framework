0a0bf4c05ce82662322e4105514c4089dfade271
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2023-05-20 09:41:14 +0330

    .

--- utils/pt/losses/net.py
+++ utils/pt/losses/net.py
@@ -13,7 +13,6 @@ class NET_Loss(Loss):
     def mse_net_loss(self, y, t):
         print(y)
         print(t)
-        assert False
         loss = self.mse(y, t)
         log = {
             'loss': loss.clone().detach().mean(),
