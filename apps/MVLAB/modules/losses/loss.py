fb2f03bb8f09626a95ec4056121fffa2eaac7dd3
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2023-05-20 10:00:58 +0330

    .

--- apps/MVLAB/modules/losses/loss.py
+++ apps/MVLAB/modules/losses/loss.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from utils.pt.losses.net import NET_Loss
+from utils.pt.losses.cgan import CGAN_Loss
 
-class Loss(NET_Loss):
+class Loss(CGAN_Loss):
     pass
