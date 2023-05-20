aeb8407232012a7dfcb94dc9427e2b8e496d8e87
Half-Blood Prince <101740277+halfbloodprincecode@users.noreply.github.com>
2023-05-20 09:33:52 +0330

    .

--- utils/pt/loss.py
+++ utils/pt/loss.py
@@ -8,7 +8,7 @@ class Loss(nn.Module):
         prefix = str(self.kwargs.get('prefix', ''))
         if prefix:
             prefix = prefix + '_'
-        setattr(self, 'forward', getattr(self, prefix + self.kwargs['netlossfn'], f'{prefix}net_loss'))
+        setattr(self, 'forward', getattr(self, prefix + self.kwargs['netlossfn'], getattr(self, f'{prefix}net_loss')))
         self.start()
     
     def start(self):
